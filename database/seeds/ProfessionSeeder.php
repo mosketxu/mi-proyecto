81ff9fa5ca284e1a73d5694bd0cd78317c8e18e5
mosketxu <mosketxu@gmail.com>
2018-05-05 10:31:55 +0200

    https://styde.net/constructor-de-consultas-sql-de-laravel/

--- database/seeds/ProfessionSeeder.php
+++ database/seeds/ProfessionSeeder.php
@@ -12,6 +12,18 @@ class ProfessionSeeder extends Seeder
     public function run()
     {
 
+        // directamente con codigo sql
+        // Entre otros problemas permite inyeccion de sql
+        // DB::insert('INSERT INTO professions (title) VALUES ("Desarrollador Back-ends")');
+
+        // directamente con codigo sql y parametros dinamicos con el componente PDO de PHP
+        // Con marcadores
+        // DB::insert('INSERT INTO professions (title) VALUES (?)',['Desarrollador Back-end3']);
+
+        // usando como marcador un parametro de sustitucion
+        // DB::insert('INSERT INTO professions (title) VALUES (:title)',['title'=>'Desarrollador Back-end4']);
+
+        // con metodo DB:insert
         DB::table('professions')->insert([
             'title'=>'Desarrollador Back-end',
         ]);
@@ -24,6 +36,13 @@ class ProfessionSeeder extends Seeder
             'title'=>'Diseñador web',
         ]);
 
+/*         // creo y borro una profession
+        DB::table('professions')->insert([
+            'title'=>'borrar',
+        ]);
+
+        DB::table('professions'
+            )->where('title', 'borrar')->delete(); */
 
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,11 +12,38 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+/*         Seleccionar de forma manual y marcadores para evitar inyeccion SQL
+        $professions=DB::select('SELECT id FROM professions WHERE title=?',['Desarrollador Back-end']);
+        dd($professions); */
+
+/*         // Seleccionar usando el constructor de consultas devuelve un objeto de la clase  Collection que encapsula el array de datos
+        $professions=DB::table('professions')
+            ->select('id')->take(1)->get();
+            // dd($professions);
+            // y asi podemos usar metodos de objetos como first
+            $professions->first();
+            dd($professions);
+ */
+
+/*         // Seleccionar con where
+        $profession=DB::table('professions')->select('id')->where('title','=','Desarrollador Back-end')->first();
+        dd($profession); */
+
+        // quitando el metodo select de DB:table se obtienen todas las columnas
+
+        // $professionId=DB::table('professions')
+        //     ->where('title','Desarrollador Back-end')
+        //     ->value('id');
+
+        $professionId=DB::table('professions')
+             ->whereTitle('Desarrollador Back-end')
+             ->value('id');
+
         DB::table('users')->insert([
             'name'=>'Alex Arregui',
             'email'=>'mosketxu@gmail.com',
             'password'=>bcrypt('laravel'),
-            // 'profession_id'=>
+            // 'profession_id'=>$professionId
         ]);
     }
 }
